f1ad9b2b29edd875ea857d9712840cca1da5cd28
Matej DB <mdrazi01@fesb.hr>
2024-02-21 17:09:43 +0100

    adding some checks in tests

--- test/Swappy.ts
+++ test/Swappy.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { Contract } from "ethers";
+import { Contract, Signer } from "ethers";
 import { ethers, upgrades } from "hardhat";
 import { SWAP_ROUTER_ADDRESS, WETH_ADDRESS, USDT_ADDRESS, QUOTER_ADDRESS } from "./addresses";
 import { parseEther } from "ethers";
@@ -21,7 +21,11 @@ describe("Swappy", function () {
     let Quoter: Contract;
     let USDT: Contract;
 
+    let owner: Signer;
+    let ownerAddress: string;
+
     before(async function () {
+
         // Deploy Swappy
         const SwappyFactory = await ethers.getContractFactory("Swappy");
         Swappy = await upgrades.deployProxy(SwappyFactory, [SWAP_ROUTER_ADDRESS, WETH_ADDRESS]);
@@ -31,6 +35,10 @@ describe("Swappy", function () {
 
         // Instantiate USDT contract
         USDT = await ethers.getContractAt(ERC20Json.abi, USDT_ADDRESS);
+
+        // Get owner address
+        [owner] = await ethers.getSigners();
+        ownerAddress = await owner.getAddress();
     })
 
     it("Should swap tokens", async function () {
@@ -38,12 +46,21 @@ describe("Swappy", function () {
         // Get info on pool dynamics - how much we should reeceive from this trade
         const quotedAmountOut = await Quoter.quoteExactInputSingle.staticCall(WETH_ADDRESS, USDT_ADDRESS, 500, parseEther("1"), 0);
 
+        // Check USDT balance before swap
+        const usdtBalanceBefore = await USDT.balanceOf(ownerAddress);
+
         /**
          * Swap 1 WETH for USDT
          * Pass `quotedAmountOut` as the minimum amount of USDT to receive
          */
         await expect(Swappy.swapEtherToToken(USDT_ADDRESS, quotedAmountOut, 500, { value: parseEther("1") }))
             .to.emit(Swappy, "SwapExecuted").withArgs(USDT_ADDRESS, parseEther("1"), quotedAmountOut, quotedAmountOut);
+
+        // Check USDT balance after swap
+        const usdtBalanceAfter = await USDT.balanceOf(ownerAddress);
+
+        // Check that USDT balance has increased by at least `quotedAmountOut`
+        expect(usdtBalanceAfter).to.be.gte(usdtBalanceBefore + quotedAmountOut);
     });
 
     it("Should not swap tokens because output amount is less than minimum", async function () {
